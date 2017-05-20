29f7c8a505298925b83e7183f6c36a48b95ed78b
moon0326 <moon.kyong@demandmedia.net>
2017-05-20 05:39:08 -0700

    fix bug

--- bin/swagger-editor-live.js
+++ bin/swagger-editor-live.js
@@ -1,35 +1,27 @@
 #!/usr/bin/env node
-/****************************************************************************
- Copyright 2016 Apigee Corporation
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
- ****************************************************************************/
 'use strict';
 
-var app = require('commander');
+var program = require('commander');
 var fs = require('fs');
+var swaggerFilePathValue;
 
-app
-  .command('serve <swaggerFile> [port]', 'Open swaggerFile in live edit mode.')
-  .action(function(swaggerFile, port) {
-    var main = require('../index.js');
-    if (fs.existsSync(swaggerFile)) {
-        main.edit(swaggerFile, port);
-    } else {
-        console.error(swaggerFile + " does not exist.");
-        process.exit(1);
-    }
-  });
+program
+    .version('1.0')
+    .arguments('<swaggerFilePath>')
+    .action(function(swaggerFilePath) {
+        swaggerFilePathValue = swaggerFilePath;
+    });
+
+program.parse(process.argv);
 
 
-app.parse(process.argv);
+if (typeof swaggerFilePathValue === 'undefined') {
+    console.error("<swaggerFilePathValue> is required. swagger-editor-live <swaggerFilePathValue> ");
+    process.exit(1);
+}
+
+if (fs.existsSync(swaggerFilePathValue)) {
+    require("../index.js").edit(swaggerFilePathValue);
+} else {
+    console.error(swaggerFilePathValue + " does not exist.");
+}
