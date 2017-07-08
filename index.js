ffc95235f92f1464c8a0b79cffc6ab457ac28a92
moon0326 <moon.kyong@demandmedia.net>
2017-07-07 17:42:42 -0700

    init

--- index.js
+++ index.js
@@ -7,48 +7,36 @@ var process = require('process');
 var open = require('open');
 var http = require('http');
 var nodeModules = path.resolve(path.resolve(__dirname, ''), 'node_modules');
-var swaggerEditorPath = path.resolve(nodeModules, 'swagger-editor');
 var app = require('connect')();
+
 
 function edit(swaggerFile, port) {
   app.use('/editor/spec', function(req, res, next) {
-    if (req.method !== 'PUT') { return next(); }
-    var stream = fs.createWriteStream(swaggerFile);
-    req.pipe(stream);
-    stream.on('finish', function() {
-      res.end('ok');
-      console.log("Saved changes");
-    })
+    switch (req.method) {
+      case 'GET': 
+        res.end(fs.readFileSync(swaggerFile));
+        break;
+      case 'PUT':
+          var stream = fs.createWriteStream(swaggerFile);
+          req.pipe(stream);
+          stream.on('finish', function() {
+            res.end('ok');
+            console.log("Saved changes");
+          })
+        break;
+      default:
+        return next();
+        break;
+    }
   });
 
-  app.use('/editor/spec', serveStatic(swaggerFile) );
-  app.use('/config/defaults.json', function(req, res, next) {
-    if (req.method !== 'GET') { return next(); }
-    res.end(JSON.stringify({
-      analytics: { google: { id: null } },
-      disableCodeGen: true,
-      disableNewUserIntro: true,
-      examplesFolder: '/spec-files/',
-      exampleFiles: [],
-      autocompleteExtension: {},
-      useBackendForStorage: true,
-      backendEndpoint: '/editor/spec',
-      backendHealthCheckTimeout: 5000,
-      useYamlBackend: true,
-      disableFileMenu: true,
-      enableTryIt: true,
-      headerBranding: false,
-      brandingCssClass: null,
-      schemaUrl: '/schema/swagger.json',
-    }));
-  });
 
-  app.use('/', serveStatic(swaggerEditorPath));
+  app.use('/', serveStatic("./"));
 
   var hostname = '127.0.0.1';
   var server = http.createServer(app);
   server.listen(port, hostname, function() {
-    open('http://' + hostname + ':' + server.address().port + '/#/edit');
+    open('http://' + hostname + ':' + server.address().port);
   });
 }
 
