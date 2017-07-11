e9be08ac88d1b1fecee636ce9c47bbd1040f61f9
Marvin Osswald <mail@marvinosswald.de>
2017-07-11 09:49:17 +0200

    New: Added --host option to be able to dockerize the package

--- bin/swagger-editor-live.js
+++ bin/swagger-editor-live.js
@@ -9,6 +9,7 @@ program
     .version('1.0')
     .arguments('<swaggerFilePath>')
     .option('-p, --port <port>', 'Port to be used. Default is 8000')
+    .option('-h, --host <Hostname|Ip>', 'Host to be used. Default is 127.0.0.1')
     .action(function(swaggerFilePath) {
         swaggerFilePathValue = swaggerFilePath;
     });
@@ -23,9 +24,12 @@ if (typeof swaggerFilePathValue === 'undefined') {
 if (typeof program.port === 'undefined') {
     program.port = 8000;
 }
+if (typeof program.host === 'undefined') {
+    program.host = "127.0.0.1";
+}
 
 if (fs.existsSync(swaggerFilePathValue)) {
-    require("../index.js").edit(swaggerFilePathValue, program.port);
+    require("../index.js").edit(swaggerFilePathValue, program.port,program.host);
 } else {
     console.error(swaggerFilePathValue + " does not exist.");
 }
